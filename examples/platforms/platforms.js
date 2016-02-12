8b780eb9047fed0aaa3a01341f348f879890edd1
Don McCurdy <don.r.mccurdy@gmail.com>
2016-02-12 00:15:09 -0500

    Add jumping ability.

--- examples/platforms/platforms.js
+++ examples/platforms/platforms.js
@@ -7,23 +7,52 @@ AFRAME.registerComponent('gamepad-controls', require('aframe-gamepad-controls'))
 AFRAME.registerComponent('keyboard-controls', require('aframe-keyboard-controls'));
 AFRAME.registerComponent('proxy-controls', require('aframe-proxy-controls'));
 
+AFRAME.registerComponent('jump-ability', require('../../src/jump-ability'));
+
+/* Bootstrap
+ —————————————————————————————————————————————*/
+
+document.addEventListener('DOMContentLoaded', function () {
+  initTerrain();
+  initGravity();
+  console.info('Platforms and gravity loaded.');
+});
+
+/* Globals
+ —————————————————————————————————————————————*/
+
+var platforms = [];
+
 /* Terrain
  —————————————————————————————————————————————*/
 
-var terrain = document.querySelector('#terrain');
+function initTerrain () {
+  var MAP_SIZE = 10,
+      PLATFORM_SIZE = 5;
 
-var v,
-    aCube;
-for (var i = 0;  i < 20; i++) {
-  v = {
-    x: Math.floor(Math.random() * 20) - 10,
-    y: Math.floor(Math.random() * 20),
-    z: Math.floor(Math.random() * 20) - 10,
-  };
-  aCube = document.createElement('a-cube');
-  aCube.setAttribute('position', v.x + ' ' + v.y + ' ' + v.z);
-  terrain.appendChild(aCube);
+  var platformsEl = document.querySelector('#platforms');
+
+  var v,
+      aCube;
+  for (var i = 0;  i < 50; i++) {
+    v = {
+      x: (Math.floor(Math.random() * MAP_SIZE) - PLATFORM_SIZE) * PLATFORM_SIZE,
+      y: (Math.floor(Math.random() * MAP_SIZE)                ) * PLATFORM_SIZE + PLATFORM_SIZE / 2,
+      z: (Math.floor(Math.random() * MAP_SIZE) - PLATFORM_SIZE) * PLATFORM_SIZE
+    };
+    aCube = document.createElement('a-cube');
+    aCube.setAttribute('position', v.x + ' ' + v.y + ' ' + v.z);
+    aCube.setAttribute('height', 5);
+    aCube.setAttribute('width', 5);
+    aCube.setAttribute('depth', 5);
+    platformsEl.appendChild(aCube);
+    platforms.push(aCube);
+  }
 }
 
 /* Gravity
  —————————————————————————————————————————————*/
+
+function initGravity () {
+  // TODO
+}
--- src/jump-ability.js
+++ src/jump-ability.js
@@ -1,16 +1,84 @@
 /**
  * Adds jump ability on component.
  */
+
+var ACCEL_G = -9.8; // m/s^2
+    
 module.exports = {
+  dependencies: ['position'],
+
+  /* Schema
+   —————————————————————————————————————————————*/
+
   schema: {
-    on: { default: 'Spacebar' },
-    distance: { default: 5 },
-    terrain: { default: '#terrain' }
+    on: { default: 'keydown:Space' },
+    playerHeight: { default: 1.764 },
+    enableDoubleJump: { default: false },
+    distance: { default: 10 }
   },
+
+  /* Init / Deinit
+   —————————————————————————————————————————————*/
+
   init: function () {
-    this.canJump = true;
+    this.position = new THREE.Vector3();
+    this.isStanding = true;
+    this.velocity = 0;
+    this.numJumps = 0;
+    
+    this.bindings = {};
+    this.bindings[this.data.on] = this.beginJump.bind(this);
+    this.el.addEventListener(this.data.on, this.bindings[this.data.on]);
   },
-  update: function () {},
-  tick: function () {},
-  remove: function () {}
+
+  remove: function () {
+    this.el.removeEventListener(this.data.on, this.bindings.beginJump);
+  },
+
+  /* Render loop
+   —————————————————————————————————————————————*/
+
+  update: function () {
+    this.tick();
+  },
+
+  tick: function (t, tDelta) {
+    if (this.isStanding) return;
+
+    this.position.copy(this.el.getAttribute('position'));
+    
+    if (this.position.y > this.data.playerHeight || this.velocity > 0) {
+      this.midJump(tDelta);
+    } else {
+      this.endJump();
+    }
+
+    this.el.setAttribute('position', this.position);
+  },
+
+  /* Jumping
+   —————————————————————————————————————————————*/
+
+  beginJump: function () {
+    if (this.isStanding || this.data.enableDoubleJump && this.numJumps < 2) {
+      this.velocity = 15;
+      this.isStanding = false;
+      this.numJumps++;
+    }
+  },
+
+  midJump: function (tDelta) {
+    this.position.y = Math.max(
+      this.position.y + this.velocity * tDelta / 300,
+      this.data.playerHeight
+    );
+    this.velocity += ACCEL_G * tDelta / 300;
+  },
+
+  endJump: function () {
+    this.position.y = this.data.playerHeight;
+    this.velocity = 0;
+    this.numJumps = 0;
+    this.isStanding = true;
+  }
 };
