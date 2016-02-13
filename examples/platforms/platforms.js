f840aa9cb8578a744ac3c0bac7f51e5fdcc84d0a
Don McCurdy <don.r.mccurdy@gmail.com>
2016-02-12 22:15:15 -0500

    Simplify platforms.js

--- examples/platforms/platforms.js
+++ examples/platforms/platforms.js
@@ -2,7 +2,7 @@ var AFRAME = require('aframe');
 // var AFRAME = window.AFRAME.aframeCore || AFRAME;
 
 /* Components
- —————————————————————————————————————————————*/
+——————————————————————————————————————————————*/
 
 AFRAME.registerComponent('gamepad-controls', require('aframe-gamepad-controls'));
 AFRAME.registerComponent('keyboard-controls', require('aframe-keyboard-controls'));
@@ -10,32 +10,19 @@ AFRAME.registerComponent('proxy-controls', require('aframe-proxy-controls'));
 
 AFRAME.registerComponent('jump-ability', require('../../src/jump-ability'));
 
-/* Bootstrap
- —————————————————————————————————————————————*/
+/* Platforms
+——————————————————————————————————————————————*/
 
 document.addEventListener('DOMContentLoaded', function () {
-  initTerrain();
-  initGravity();
-  console.info('Platforms and gravity loaded.');
-});
-
-/* Globals
- —————————————————————————————————————————————*/
-
-var platforms = [];
-
-/* Terrain
- —————————————————————————————————————————————*/
-
-function initTerrain () {
   var MAP_SIZE = 10,
-      PLATFORM_SIZE = 5;
+      PLATFORM_SIZE = 5,
+      NUM_PLATFORMS = 50;
 
   var platformsEl = document.querySelector('#platforms');
 
   var v,
       aCube;
-  for (var i = 0;  i < 50; i++) {
+  for (var i = 0;  i < NUM_PLATFORMS; i++) {
     v = {
       x: (Math.floor(Math.random() * MAP_SIZE) - PLATFORM_SIZE) * PLATFORM_SIZE,
       y: (Math.floor(Math.random() * MAP_SIZE)                ) * PLATFORM_SIZE + PLATFORM_SIZE / 2,
@@ -43,17 +30,11 @@ function initTerrain () {
     };
     aCube = document.createElement('a-cube');
     aCube.setAttribute('position', v.x + ' ' + v.y + ' ' + v.z);
-    aCube.setAttribute('height', 5);
-    aCube.setAttribute('width', 5);
-    aCube.setAttribute('depth', 5);
+    aCube.setAttribute('height', PLATFORM_SIZE);
+    aCube.setAttribute('width', PLATFORM_SIZE);
+    aCube.setAttribute('depth', PLATFORM_SIZE);
     platformsEl.appendChild(aCube);
-    platforms.push(aCube);
   }
-}
 
-/* Gravity
- —————————————————————————————————————————————*/
-
-function initGravity () {
-  // TODO
-}
+  console.info('Platforms loaded.');
+});
