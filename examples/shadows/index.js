7a3dc0979c46bdd548cfcb3fb5e4ab5044668cc5
Don McCurdy <don.r.mccurdy@gmail.com>
2016-03-07 21:37:06 -0500

    Move shadows js

--- examples/shadows/index.js
+++ examples/shadows/index.js
@@ -8,5 +8,5 @@ AFRAME.registerComponent('keyboard-controls', require('aframe-keyboard-controls'
 AFRAME.registerComponent('proxy-controls', require('aframe-proxy-controls'));
 AFRAME.registerComponent('jump-ability', require('../../src/jump-ability'));
 AFRAME.registerComponent('obj-loader', require('aframe-obj-loader-component'));
-AFRAME.registerComponent('shadow-light', require('../../src/shadow-light'));
-AFRAME.registerComponent('shadow', require('../../src/shadow'));
+AFRAME.registerComponent('shadow-light', require('../../src/shadows/shadow-light'));
+AFRAME.registerComponent('shadow', require('../../src/shadows/shadow'));
