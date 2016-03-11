2a021f234e259a6b9335128b770a3386c96d037f
Don McCurdy <don.r.mccurdy@gmail.com>
2016-03-10 21:25:45 -0500

    Implement hmd-controls

--- src/controls/hmd-controls.js
+++ src/controls/hmd-controls.js
@@ -1,10 +1,93 @@
+var TICK_DEBOUNCE = 4; // ms
+
 module.exports = {
-  init: function () {},
+
+  /*******************************************************************
+   * Schema
+   */
+
+  schema: {
+    enabled: { default: true }
+  },
+
+  /*******************************************************************
+   * Lifecycle
+   */
+
+  init: function () {
+    this.tPrev = 0;
+    this.dolly = new THREE.Object3D();
+    this.euler = new THREE.Euler();
+    this.controls = new THREE.VRControls(this.dolly);
+    this.zeroQuaternion = new THREE.Quaternion();
+
+    if (this.el.sceneEl.addBehavior) {
+      this.el.sceneEl.addBehavior(this);
+    }
+  },
+
   remove: function () {},
 
-  update: function () {},
-  tick: function () {},
+  /*******************************************************************
+   * Tick
+   */
 
-  isRotationActive: function () { return false; },
-  getRotation: function () {}
+  update: function () { this.tick(); },
+
+  tick: function (t) {
+    t = t || Date.now();
+    if (t - this.tPrev > TICK_DEBOUNCE) {
+      this.controls.update();
+      this.tPrev = t;
+    }
+  },
+
+  /*******************************************************************
+   * Universal interface
+   */
+
+  isRotationActive: function () {
+    return this.data.enabled && !this.dolly.quaternion.equals(this.zeroQuaternion);
+  },
+
+  getRotation: (function () {
+    var hmdEuler = new THREE.Euler();
+    hmdEuler.order = 'YXZ';
+    return function () {
+      this.tick();
+      var hmdQuaternion = this.calculateHMDQuaternion();
+      hmdEuler.setFromQuaternion(hmdQuaternion);
+      return new THREE.Vector3(
+        THREE.Math.radToDeg(hmdEuler.x),
+        THREE.Math.radToDeg(hmdEuler.y),
+        THREE.Math.radToDeg(hmdEuler.z)
+      );
+    };
+  }()),
+
+  /*******************************************************************
+   * Orientation
+   */
+
+  calculateHMDQuaternion: (function () {
+    var hmdQuaternion = new THREE.Quaternion();
+    return function () {
+      var dolly = this.dolly;
+      if (!this.zeroed && !dolly.quaternion.equals(this.zeroQuaternion)) {
+        this.zeroOrientation();
+        this.zeroed = true;
+      }
+      hmdQuaternion.copy(this.zeroQuaternion).multiply(dolly.quaternion);
+      return hmdQuaternion;
+    };
+  }()),
+
+  zeroOrientation: function () {
+    var euler = new THREE.Euler();
+    euler.setFromQuaternion(this.dolly.quaternion.clone().inverse());
+    // Cancel out roll and pitch. We want to only reset yaw
+    euler.z = 0;
+    euler.x = 0;
+    this.zeroQuaternion.setFromEuler(euler);
+  }
 };
