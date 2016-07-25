3453db79bfa51c0e882784e85150476757e31612
Don McCurdy <don.r.mccurdy@gmail.com>
2016-07-24 22:55:57 -0400

    Fixes for physics system.

--- src/math/velocity.js
+++ src/math/velocity.js
@@ -27,13 +27,13 @@ module.exports = {
   step: function (t, dt) {
     if (!dt) return;
 
-    var physics = this.el.sceneEl.systems.physics || {maxInterval: 1 / 60},
+    var physics = this.el.sceneEl.systems.physics || {data: {maxInterval: 1 / 60}},
 
         // TODO - There's definitely a bug with getComputedAttribute and el.data.
         velocity = this.el.getAttribute('velocity') || {x: 0, y: 0, z: 0},
         position = this.el.getAttribute('position') || {x: 0, y: 0, z: 0};
 
-    dt = Math.min(dt, physics.maxInterval * 1000);
+    dt = Math.min(dt, physics.data.maxInterval * 1000);
 
     this.el.setAttribute('position', {
       x: position.x + velocity.x * dt / 1000,
--- src/physics/system/physics.js
+++ src/physics/system/physics.js
@@ -8,20 +8,20 @@ var CANNON = require('cannon'),
  */
 module.exports = {
   schema: {
-    gravity:                            { default: C_GRAV },
-    friction:                           { default: C_MAT.friction },
-    restitution:                        { default: C_MAT.restitution },
-    contactEquationStiffness:           { default: C_MAT.contactEquationStiffness },
-    contactEquationRelaxation:          { default: C_MAT.contactEquationRelaxation },
-    frictionEquationStiffness:          { default: C_MAT.frictionEquationStiffness },
-    frictionEquationRegularization:     { default: C_MAT.frictionEquationRegularization },
+    gravity:                        { default: C_GRAV },
+    friction:                       { default: C_MAT.friction },
+    restitution:                    { default: C_MAT.restitution },
+    contactEquationStiffness:       { default: C_MAT.contactEquationStiffness },
+    contactEquationRelaxation:      { default: C_MAT.contactEquationRelaxation },
+    frictionEquationStiffness:      { default: C_MAT.frictionEquationStiffness },
+    frictionEquationRegularization: { default: C_MAT.frictionEquationRegularization },
 
     // Never step more than four frames at once. Effectively pauses the scene
     // when out of focus, and prevents weird "jumps" when focus returns.
-    maxInterval:      { default: 4 / 60 },
+    maxInterval:                    { default: 4 / 60 },
 
     // If true, show wireframes around physics bodies.
-    debug:        { default: false },
+    debug:                          { default: false },
   },
 
   /**
@@ -53,7 +53,7 @@ module.exports = {
     this.world.quatNormalizeFast = false;
     // this.world.solver.setSpookParams(300,10);
     this.world.solver.iterations = CONSTANTS.ITERATIONS;
-    this.world.gravity.set(0, data.GRAVITY, 0);
+    this.world.gravity.set(0, data.gravity, 0);
     this.world.broadphase = new CANNON.NaiveBroadphase();
 
     this.material = new CANNON.Material({name: 'defaultMaterial'});
