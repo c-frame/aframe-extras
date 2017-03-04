432344a1d8c42ba89b2efa364eaa6164e1c1a203
Don McCurdy <don.r.mccurdy@gmail.com>
2017-03-04 16:22:31 -0500

    Use 'userHeight=1.6' in all examples.
    Fixes #118.

--- src/controls/checkpoint-controls.js
+++ src/controls/checkpoint-controls.js
@@ -57,8 +57,6 @@ module.exports = {
 
     position.copy(this.el.getAttribute('position'));
     targetPosition.copy(this.checkpoint.object3D.getWorldPosition());
-    // TODO - Cleverer ways around this?
-    targetPosition.y = position.y;
     offset.copy(targetPosition).sub(position);
   }
 };
