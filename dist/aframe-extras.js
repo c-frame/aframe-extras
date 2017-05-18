3f214659a19c7ce44abd3d5183d8c6f0c3830f18
Don McCurdy <don.r.mccurdy@gmail.com>
2017-05-17 19:43:14 -0700

    3.8.2

--- dist/aframe-extras.js
+++ dist/aframe-extras.js
@@ -21240,13 +21240,17 @@ module.exports = {
   },
 
   onKeyDown: function (event) {
-    this.localKeys[event.code] = true;
-    this.emit(event);
+    if (AFRAME.utils.shouldCaptureKeyEvent(event)) {
+      this.localKeys[event.code] = true;
+      this.emit(event);
+    }
   },
 
   onKeyUp: function (event) {
-    delete this.localKeys[event.code];
-    this.emit(event);
+    if (AFRAME.utils.shouldCaptureKeyEvent(event)) {
+      delete this.localKeys[event.code];
+      this.emit(event);
+    }
   },
 
   onBlur: function () {
