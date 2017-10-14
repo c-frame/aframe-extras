6b5347846c95683ae25a8684808a79d34e784db4
Don McCurdy <don.r.mccurdy@gmail.com>
2017-10-14 14:24:05 -0700

    [gltf-model-legacy] Remove unneeded shader updates.
    Fixes #179.

--- src/loaders/gltf-model-legacy.js
+++ src/loaders/gltf-model-legacy.js
@@ -31,7 +31,6 @@ module.exports.Component = {
       this.loader.load(src, function gltfLoaded (gltfModel) {
         self.model = gltfModel.scene;
         self.model.animations = gltfModel.animations;
-        self.system.registerModel(self.model);
         el.setObject3D('mesh', self.model);
         el.emit('model-loaded', {format: 'gltf', model: self.model});
       });
@@ -41,46 +40,6 @@ module.exports.Component = {
   remove: function () {
     if (!this.model) { return; }
     this.el.removeObject3D('mesh');
-    this.system.unregisterModel(this.model);
-  }
-};
-
-/**
- * glTF model system.
- */
-module.exports.System = {
-  init: function () {
-    this.models = [];
-  },
-
-  /**
-   * Updates shaders for all glTF models in the system.
-   */
-  tick: function () {
-    var sceneEl = this.sceneEl;
-    if (sceneEl.hasLoaded && this.models.length) {
-      THREE.GLTFLoader.Shaders.update(sceneEl.object3D, sceneEl.camera);
-    }
-  },
-
-  /**
-   * Registers a glTF asset.
-   * @param {object} gltf Asset containing a scene and (optional) animations and cameras.
-   */
-  registerModel: function (gltf) {
-    this.models.push(gltf);
-  },
-
-  /**
-   * Unregisters a glTF asset.
-   * @param  {object} gltf Asset containing a scene and (optional) animations and cameras.
-   */
-  unregisterModel: function (gltf) {
-    var models = this.models;
-    var index = models.indexOf(gltf);
-    if (index >= 0) {
-      models.splice(index, 1);
-    }
   }
 };
 
